745cd5e945a359ae663bc73ff0fe1c6f84430738
Alex Pashkov <56487358+Mackrage@users.noreply.github.com>
2021-07-30 10:49:13 +0300

    KONTINENT-292

--- backend/garpix_instagram/instagram.py
+++ backend/garpix_instagram/instagram.py
@@ -5,33 +5,40 @@ from .models import InstagramHashtag, InstagramPost  # noqa
 import shutil
 
 
-class Instagram(object):
-
+class Instagram:
     def __init__(self, username=os.getenv('INSTAGRAM_USERNAME'), password=os.getenv('INSTAGRAM_PASSWORD')):
         self.username = username
         self.password = password
         cookie_del = glob.glob("config/*cookie.json")
         if cookie_del:
             os.remove(cookie_del[0])
-        shutil.rmtree('config')
+        shutil.rmtree('config', ignore_errors=True)
         bot = Bot()
         bot.login(username=self.username, password=self.password)
         self.bot = bot
 
     def update_base(self):
-
         self.get_posts_by_hashtags()
 
     def get_posts_by_hashtags(self):
-
         hashtags = InstagramHashtag.objects.all()
         hashtag_list = []
+        instagram_posts_bulk_create = []
         for hashtag in hashtags:
             hashtag_list.append(hashtag.title)
-        posts = self.bot.get_total_hashtag_medias(hashtag_list)
-        for post in posts:
-            media_info = self.bot.get_media_info(post)
-            print(media_info)
-            # InstagramPost.objects.create(user_name=post.owner_username, like_count=post.likes,
-            #                              user_link=post.url, user_image=post.owner_profile.profile_pic_url,
-            #                              post_image=post)
+            posts = self.bot.get_hashtag_medias('cat', filtration=False)
+
+            for post in posts:
+                media_info = self.bot.get_media_info(post)[0]
+                username = media_info['user']['username']
+                kwargs = {
+                    'user_name': username,
+                    'user_image': media_info['user']['profile_pic_url'],
+                    'user_link': 'https://www.instagram.com/' + username + '/',
+                    'post_link': f'https://www.instagram.com/p/{media_info["code"]}/',
+                    'like_count': media_info['like_count'],
+                    'post_image': media_info['image_versions2']['candidates'][0]['url'],
+                }
+                instagram_post = InstagramPost(**kwargs)
+                instagram_posts_bulk_create.append(instagram_post)
+        InstagramPost.objects.bulk_create(instagram_posts_bulk_create)
