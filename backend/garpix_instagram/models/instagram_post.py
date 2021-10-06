7bafb1be18ad57072f7cbad52fe2596057ffd333
oem <azarov@garpix.com>
2021-10-06 17:57:55 +0300

    1.1.1 update

--- backend/garpix_instagram/models/instagram_post.py
+++ backend/garpix_instagram/models/instagram_post.py
@@ -9,14 +9,11 @@ InstagramPostMixin = import_string(settings.GARPIX_INSTAGRAM_POST_MIXIN)
 
 class InstagramPost(InstagramPostMixin, models.Model):
     user_name = models.CharField(max_length=250, verbose_name='Ник пользователя', null=True)
-    user_image = models.ImageField(verbose_name="Изображение пользователя",  null=True,
+    user_image = models.ImageField(verbose_name="Изображение пользователя", null=True,
                                    upload_to=get_file_path)
     post_link = models.CharField(max_length=1000, verbose_name='Ссылка на пост', null=True)
-    post_image = models.ImageField(verbose_name="Изображение поста",  null=True, upload_to=get_file_path)
-    like_count = models.CharField(max_length=15, verbose_name='Количество лайков', null=True,
-                                  validators=[
-                                      RegexValidator(r'^[0-9]*$', 'Значение должно содержать только цифры'),
-                                  ])
+    post_image = models.ImageField(verbose_name="Изображение поста", null=True, upload_to=get_file_path)
+    like_count = models.PositiveIntegerField(verbose_name='Количество лайков', null=True)
     active = models.BooleanField(default=False, verbose_name='Отображать пост?')
 
     def __str__(self):
