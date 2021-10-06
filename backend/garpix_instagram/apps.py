a37007ba14092b7aee94c1b80576ff14cda5380a
oem <azarov@garpix.com>
2021-10-06 18:06:23 +0300

    1.1.1 update

--- backend/garpix_instagram/apps.py
+++ backend/garpix_instagram/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class GarpixInstagramConfig(AppConfig):
     name = 'garpix_instagram'
+    verbose_name = 'Garpix Instagram'
+
+    def ready(self):
+        import property.signals  # noqa
